f9eb47f74120912c59c71d356b3e5470716e9f55
chawlabhi <abhishekchawla29@gmail.com>
2020-10-07 17:29:13 +0530

    Updates

---
+++ precache-manifest.299c63253bdff2963ad05f4feae696d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9005d8d61848df0717ac535b8c198c19",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "c1d7e67e82229ed19f53",
+    "url": "/Portfolio/static/css/main.0be18c04.chunk.css"
+  },
+  {
+    "revision": "10012f51f03b9c726608",
+    "url": "/Portfolio/static/js/2.d529bc05.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/Portfolio/static/js/2.d529bc05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1d7e67e82229ed19f53",
+    "url": "/Portfolio/static/js/main.27cf839b.chunk.js"
+  },
+  {
+    "revision": "8c6ada5d8e3eab81b7ac",
+    "url": "/Portfolio/static/js/runtime-main.6437dfda.js"
+  }
+]);
